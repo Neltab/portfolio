b989db97652a2d4f1b817f51800f722fdf3b5880
Aurelien B <61365539+Neltab@users.noreply.github.com>
2025-09-21 22:50:26 +0200

    fix: import optimisations

--- src/controls.ts
+++ src/controls.ts
@@ -1,4 +1,4 @@
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import camera from "./camera";
 import canvas from "./canvas";
 
--- src/meshes/garden.ts
+++ src/meshes/garden.ts
@@ -1,11 +1,11 @@
 import loadPointCloud from "../utils/loadPointCloud";
 import { updateLoop } from "../timer";
-import * as THREE from "three";
+import { MathUtils } from "three/src/math/MathUtils.js";
 
 const gardenGroup = await loadPointCloud('/static/models/garden4-2.glb', [0.075], [4]);
 const garden = gardenGroup.children[0];
-garden.rotateX(THREE.MathUtils.degToRad(90));
-garden.rotateZ(THREE.MathUtils.degToRad(25));
+garden.rotateX(MathUtils.degToRad(90));
+garden.rotateZ(MathUtils.degToRad(25));
 
 updateLoop.push((elapsedTime) => {
     garden.material.uniforms.time.value = elapsedTime;
